36113a9458d1b77c8043fb415b99f7869b25a1e5
ivalrivall <ivalrival95@gmail.com>
2022-09-11 15:00:52 +0700

    fix: change cron

--- clevercloud/cron.sh
+++ clevercloud/cron.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -l
-set -euo pipefail
+# set -euo pipefail
 
-pushd "$APP_HOME"
+# pushd "$APP_HOME"
+cd ${APP_HOME} # Which has been loaded by the env.
 php artisan schedule:run >> /dev/null 2>&1
