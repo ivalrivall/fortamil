755084a18d5fd6f523d201f6769f64b3cf00f608
ivalrivall <ivalrival95@gmail.com>
2022-09-24 18:21:54 +0700

    fix: fixing make visible

--- app/Repositories/WarehouseRepository.php
+++ app/Repositories/WarehouseRepository.php
@@ -115,7 +115,9 @@ class WarehouseRepository extends BaseRepository implements WarehouseRepositoryI
             $per_page = 10;
         }
 
-        return $data->simplePaginate($per_page)->makeVisible(['deleted_at']);
+        $data = $data->simplePaginate($per_page);
+        $data->makeVisible(['deleted_at']);
+        return $data;
     }
 
     /**
